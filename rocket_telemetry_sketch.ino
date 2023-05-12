85b32b81fda3f4a2d3f0fe8148dbb1013a7b774a
Edgar Butwilowski <postfach@mail.ch>
2023-05-12 22:39:34 +0200

    First working rocket telemetry sketch

--- rocket_telemetry_sketch.ino
+++ rocket_telemetry_sketch.ino
@@ -1,25 +1,40 @@
-#include <Adafruit_BMP280.h>
+#include <Wire.h>
 #include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
 #include <LoRa.h>
 
-Adafruit_BMP280 bmp280;
+Adafruit_BMP3XX bmp388;
 
-void setup() {
-  Serial.begin(9600);
-  while(!Serial){}
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
   Serial.println("Serial port ready");
-  while(!LoRa.begin(868E6)){
+  while (!LoRa.begin(868E6))
+  {
     Serial.println("Long range radio NOT working");
     delay(1000);
   }
-  while(!bmp280.begin()){
-    Serial.println("BMP NOT working 1");
+  int i = 1;
+  while (!bmp388.begin_I2C())
+  {
+    Serial.print("BMP NOT working ");
+    Serial.println(i);
+    i++;
     delay(1000);
   }
 
+  bmp388.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp388.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp388.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp388.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Serial is still working");
 
   Serial.println("Sending long range radio packet");
@@ -28,14 +43,18 @@ void loop() {
   LoRa.print("test");
   LoRa.endPacket();
 
+  if (!bmp388.performReading())
+  {
+    Serial.println("Could not read pressure sensor");
+  }
+
   Serial.print("Temperatur: ");
-  Serial.print(bmp280.readTemperature());
+  Serial.print(bmp388.temperature);
   Serial.println(" C");
 
   Serial.print("Luftdruck: ");
-  Serial.print(bmp280.readPressure() / 100.0);
+  Serial.print(bmp388.pressure / 100.0);
   Serial.println(" hPa");
 
   delay(1000);
-
 }
