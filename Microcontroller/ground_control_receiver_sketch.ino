d9c783a3f7687970b28e62e37d58c67e935f817b
Edgar Butwilowski <postfach@mail.ch>
2023-05-26 18:36:53 +0200

    Cleaned firmware code a bit

--- Microcontroller/ground_control_receiver_sketch.ino
+++ Microcontroller/ground_control_receiver_sketch.ino
@@ -6,44 +6,32 @@ void setup()
     while (!Serial)
     {
     }
-    Serial.println("Serial port ready");
     while (!LoRa.begin(868E6))
     {
-        Serial.println("Long range radio NOT working");
+        Serial.println("ERR-1");
         delay(1000);
     }
 }
 
 void loop()
 {
-    Serial.println("Serial is still working");
-
-    Serial.println("Trying to receive long range radio packet");
-
     int packetSize = LoRa.parsePacket();
 
     if (packetSize != 0)
     {
-        Serial.println("Receiving long range radio packet");
         int recipientId = LoRa.read();
-        Serial.print("recipientId: ");
-        Serial.println(recipientId);
-
-        String message = "";
-        while (LoRa.available())
-        {
-            Serial.println("Getting message");
-            message += (char)LoRa.read();
-        }
-
         if (recipientId == 0xCF)
         {
-            Serial.print("message: ");
+            String message = "";
+            while (LoRa.available())
+            {
+                message += (char)LoRa.read();
+            }
             Serial.println(message);
         }
         else
         {
-            Serial.println("Message not for me");
+            Serial.println("ERR-2");
         }
     }
 }
--- Microcontroller/rocket_telemetry_sketch.ino
+++ Microcontroller/rocket_telemetry_sketch.ino
@@ -3,72 +3,54 @@
 #include <LoRa.h>
 
 byte destinationId = 0xCF;
+float averageAltitude = 0.0;
 
 Adafruit_BMP3XX bmp388;
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
-  {
-  }
-  Serial.println("Serial port ready");
   while (!LoRa.begin(868E6))
-  {
-    Serial.println("Long range radio NOT working");
     delay(1000);
-  }
-  int i = 1;
   while (!bmp388.begin_I2C())
-  {
-    Serial.print("BMP NOT working ");
-    Serial.println(i);
-    i++;
     delay(1000);
-  }
 
   bmp388.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp388.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp388.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp388.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  while (!bmp388.performReading())
+  {
+  }
+
+  calibrateAltitude();
 }
 
 void loop()
 {
-  Serial.println("Serial is still working");
-
-  Serial.println("Sending long range radio packet");
+  while (!bmp388.performReading())
+  {
+  }
 
   LoRa.beginPacket();
   LoRa.write(destinationId);
-  LoRa.print("test");
+
+  LoRa.print(bmp388.temperature);
+  Serial.print(";");
+  LoRa.print(bmp388.pressure);
+  Serial.println(";");
+  LoRa.print(bmp388.readAltitude(1020.0)); // 1020 hPa ist der Luftdruck auf Meereshoehe
+
   LoRa.endPacket();
 
-  if (!bmp388.performReading())
-  {
-    Serial.println("Could not read pressure sensor");
-  }
-
-  Serial.print("Temperatur: ");
-  Serial.print(bmp388.temperature);
-  Serial.println(" C");
-
-  Serial.print("Luftdruck: ");
-  Serial.print(bmp388.pressure / 100.0);
-  Serial.println(" hPa");
-
-  Serial.print("Hoehe: ");
-  Serial.print(bmp388.readAltitude(1020.0)); // 1020 hPa ist der Luftdruck auf Meereshoehe
-  Serial.println(" m");
-
-  delay(1000);
+  delay(500);
 }
 
-
-float calibrateAltitude(){
-  float averageAltitude = 0.0;
-  for(int i = 0; i < 10; i++){
+void calibrateAltitude()
+{
+  for (int i = 0; i < 10; i++)
+  {
     averageAltitude += bmp388.readAltitude(1020.0); // 1020 hPa ist der Luftdruck auf Meereshoehe
   }
-  return averageAltitude / 10;
+  averageAltitude = averageAltitude / 10;
 }
