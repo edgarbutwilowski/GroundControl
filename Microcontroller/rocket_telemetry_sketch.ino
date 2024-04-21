ddb8ebe6ac3ea4b96493a0e1a4ce4d8a613b8338
Edgar Butwilowski <postfach@mail.ch>
2024-04-21 13:59:37 +0200

    Rocket telemetry module now calculates the height itself

--- Microcontroller/rocket_telemetry_sketch.ino
+++ Microcontroller/rocket_telemetry_sketch.ino
@@ -3,7 +3,7 @@
 #include <LoRa.h>
 
 byte destinationId = 0xCF;
-float averageAltitude = 0.0;
+double averagePressure = 0.0;
 
 Adafruit_BMP3XX bmp388;
 
@@ -14,43 +14,43 @@ void setup()
   while (!bmp388.begin_I2C())
     delay(1000);
 
-  bmp388.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp388.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp388.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp388.setOutputDataRate(BMP3_ODR_50_HZ);
+  bmp388.setTemperatureOversampling(BMP3_NO_OVERSAMPLING);
+  bmp388.setPressureOversampling(BMP3_NO_OVERSAMPLING);
+  bmp388.setIIRFilterCoeff(BMP3_IIR_FILTER_DISABLE);
+  bmp388.setOutputDataRate(BMP3_ODR_25_HZ);
 
   while (!bmp388.performReading())
-  {
-  }
+    delay(1000);
 
-  calibrateAltitude();
+  calibratePressure();
 }
 
 void loop()
 {
   while (!bmp388.performReading())
-  {
-  }
+    delay(1000);
 
   LoRa.beginPacket();
   LoRa.write(destinationId);
 
-  LoRa.print(bmp388.temperature);
-  Serial.print(";");
-  LoRa.print(bmp388.pressure);
-  Serial.println(";");
-  LoRa.print(bmp388.readAltitude(1020.0)); // 1020 hPa ist der Luftdruck auf Meereshoehe
+  double height = (averagePressure - bmp388.pressure) * 8.0; // 1hPa pro 8 Meter
+
+  LoRa.println(height);
+
+  Serial.println(height);
 
   LoRa.endPacket();
 
   delay(500);
 }
 
-void calibrateAltitude()
+void calibratePressure()
 {
   for (int i = 0; i < 10; i++)
   {
-    averageAltitude += bmp388.readAltitude(1020.0); // 1020 hPa ist der Luftdruck auf Meereshoehe
+    while (!bmp388.performReading())
+      delay(1000);
+    averagePressure += bmp388.pressure;
   }
-  averageAltitude = averageAltitude / 10;
+  averagePressure = averagePressure / 10.0;
 }
